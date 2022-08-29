700753ca222a5d9e9861d48b1c2e967df5521a7b
Noble Okafor <okafornoble202@gmail.com>
2022-08-29 16:45:51 +0100

    background location issue

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import LoadScreen from "./screens/LoadScreen"
-import MapScreen from "./screens/MapScreen"
-
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import LoadScreen from "./screens/LoadScreen";
+import MapScreen from "./screens/MapScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator 
-      screenOptions = {{
-      headerShown: false
-    }}>
-        <Tab.Screen name = "Load" component = {LoadScreen}/>
-        <Tab.Screen name = "Map" component = {MapScreen}/>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen name="Load" component={LoadScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,25 +1,60 @@
-import * as React from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
+  const [errMsg, setErrMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  const requestLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrMsg("Permission denied");
+
+      return;
+    } else if (status == "granted") {
+      setErrMsg("Permission granted");
+      let devicelocation = await Location.getCurrentPositionAsync({});
+      setLocation(devicelocation);
+      return;
+    }
+  };
+  useEffect(() => {
+    requestLocation();
+  }, []);
+
+  let text = "requesting permission!";
+  if (errMsg) {
+    text = errMsg;
+  }
+
+  let deviceLattitude = location.coords.latitude;
+  let deviceLongitude = location.coords.longitude;
+
+  console.log(location);
+
   return (
     <View style={styles.container}>
-       <MapView
+      <Text>{text}</Text>
+      <MapView
         initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+          latitude: deviceLattitude,
+          longitude: deviceLongitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
         style={styles.map}
-       >
-        <Marker
-            coordinate={{ latitude : 37.78825 , longitude : -122.4324 }}
-        />
-        </MapView>
+      >
+        {/* <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+        /> */}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,12 +63,12 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  }
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
